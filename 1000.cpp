124fb0eced19df6c8c754bc4ff8ca804fd192a80
developersung13 <developersung13@gmail.com>
2021-12-28 23:51:15 +0900

    commit question no.1000

---
+++ 1000.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main(int argc, const char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n;
+    cout << (n&1);
+
+    return 0;
+}
